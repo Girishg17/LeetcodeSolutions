4da65e39af3e94dba8eb54ba97d2ac5bf0b26c42
GIRISH G <91957405+Girishg17@users.noreply.github.com>
2023-09-25 11:07:37 +0530

    Time: 6 ms (19.69%), Space: 7.1 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int j=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]!=s[j])return t[i];
+            j++;
+            
+        }
+        return 'a';
+        
+    }
+};
