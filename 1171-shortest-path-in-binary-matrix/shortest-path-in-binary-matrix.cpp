fea777c5929b7ecf5c31818cb6c6761a67388fca
GIRISH G <91957405+Girishg17@users.noreply.github.com>
2025-01-24 16:16:50 +0530

    Time: 13 ms (80.97%) | Memory: 25.5 MB (52.94%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,34 +5,31 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<int, int>> q;
+        queue<pair<int, int>> st;
         int dir[8][8] = {{0, 1}, {1, 0},   {0, -1}, {-1, 0},
                          {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-        q.push({0, 0});
-        vis[0][0] = 1;
-        int steps = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                int row = q.front().first;
-                int col = q.front().second;
-                if (row == n - 1 && col == m - 1)
-                    return steps+1;
-                q.pop();
-                for (int i = 0; i < 8; i++) {
-                    int newrow = row + dir[i][0];
-                    int newcol = col + dir[i][1];
-                    // if(newrow==n-1 && newcol==m-1)return steps;
-                    if (newrow < n && newcol < m && newrow >= 0 &&
-                        newcol >= 0 && vis[newrow][newcol] == 0 &&
-                        grid[newrow][newcol] == 0) {
-                        q.push({newrow, newcol});
-                        vis[newrow][newcol] = 1;
-                    }
+       st.push({0,0});
+       vis[0][0]=1;
+       int ans=1;
+       while(!st.empty()){
+         int size=st.size();
+         while(size--){
+            int row=st.front().first;
+            int col=st.front().second;
+            if(row==n-1 && col==m-1)return ans;
+           
+            st.pop();
+            for(auto it:dir){
+                int newrow=row+it[0];
+                int newcol=col+it[1];
+                if(newrow>=0 && newcol>=0 && newrow<n && newcol<m && vis[newrow][newcol]==0 && grid[newrow][newcol]==0){
+                    st.push({newrow,newcol});
+                    vis[newrow][newcol]=1;
                 }
             }
-            steps++;
-        }
-        return -1;
+         }
+         ans++;
+       }
+       return -1;
     }
 };
