00d899973590011d902fd97a3d8eb5cf872407ce
GIRISH G <91957405+Girishg17@users.noreply.github.com>
2025-03-19 10:34:44 +0530

    Time: 3 ms (89.05%) | Memory: 139.9 MB (74.50%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+     int c=0;
+     int i=0;
+     while(i<nums.size()){
+        if(nums[i]==0){
+            int k=0;
+            while(i+2<nums.size() && i+k<nums.size() && k<=2){
+                
+                nums[i+k]=!nums[i+k];
+                k++;
+            }
+            c++;
+        }
+        i++;
+     }
+     for(int j=0;j<nums.size();j++){
+        if(nums[j]==0)return -1;
+     }
+     return c;
+    }
+};
