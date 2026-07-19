9ff44e34f515e8eba1c61087aad9c5ea65fe8fe4
GIRISH <91957405+Girishg17@users.noreply.github.com>
2026-07-19 22:02:27 +0530

    Time: 28 ms (49.21%) | Memory: 17.4 MB (52.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& grid,int r,int c,int m,int n){
-        if(r<0 || c <0 || r>=m || c>=n)return;
-        int dir[4][2]={{0,1},{1,0},{-1,0},{0,-1}};
-        grid[r][c]='0';
-        for(auto it:dir){
-                int newrow=r+it[0];
-                int newcol=c+it[1];
-                if(newrow >=0 && newcol>=0 && newrow<m && newcol<n && grid[newrow][newcol]=='1'){
-                    solve(grid,newrow,newcol,m,n);
-                }
-
+    void solve(int m,int n,int i, int j,vector<vector<char>>& grid){
+        if(i<0 || j<0 || i>=m ||j>=n|| grid[i][j]=='0' || grid[i][j]=='2')return;
+        grid[i][j]='2';
+        int arr[4][4]={{0,1},{1,0},{-1,0},{0,-1}};
+        for(auto &it:arr){
+            int new_row=it[0]+i;
+            int new_col=it[1]+j;
+            solve(m,n,new_row,new_col,grid);
         }
         return;
+
     }
     int numIslands(vector<vector<char>>& grid) {
         int ans=0;
         int m=grid.size();
         int n=grid[0].size();
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++)
+        {
+     
             for(int j=0;j<n;j++){
+ 
                 if(grid[i][j]=='1'){
-                    ans++;
-                    solve(grid,i,j,m,n);
-                }
 
+                    solve(m,n,i,j,grid);
+                    ans++;
+                }
             }
         }
         return ans;
